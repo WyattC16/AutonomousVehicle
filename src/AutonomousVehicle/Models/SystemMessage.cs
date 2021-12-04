2881e2080a363c11ea60adc57b8e9c0838c2557d
Wyatt Condon <wyatt.condon@gmail.com>
2021-12-04 14:49:45 -0500

    add order to System Message

--- src/AutonomousVehicle/Models/SystemMessage.cs
+++ src/AutonomousVehicle/Models/SystemMessage.cs
@@ -5,4 +5,6 @@ public class SystemMessage
     public SystemMessageCriticality MessageCriticality { get; set; }
 
     public string Message { get; set; } = string.Empty;
+
+    public int Order { get; set; }
 }
